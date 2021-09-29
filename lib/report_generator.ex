c11c5619e099abd7eed89120ac5138957532b29b
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-09-28 23:57:30 -0300

    Reading the file using File.stream

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -2,10 +2,15 @@ defmodule ReportGenerator do
 
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 end
