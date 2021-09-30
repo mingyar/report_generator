c45e43fff8472df613f21a3a30df5ad9edeac4a5
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-09-30 00:32:10 -0300

    Extracting parser to a new module

---
+++ lib/parser.ex
@@ -0,0 +1,16 @@
+defmodule ReportGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+end
